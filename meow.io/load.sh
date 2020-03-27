32bb1df21c59e72d158be27e8a7e2f311cee68c9
Chris Parnin <chris.parnin@gmail.com>
2020-03-27 17:52:04 -0400

    Add votes

--- meow.io/load.sh
+++ meow.io/load.sh
@@ -4,7 +4,7 @@ do
 
     seq 1 10 | xargs -I {} -n1 -P4 curl -s localhost:3000/ >/dev/null
     #curl --request POST -H "Content-Type: application/json" --data '{"score": 1}' localhost:3000/fact/33/vote
-    # seq 5 18 | xargs -I {} -n1 -P5 curl --request POST -H "Content-Type: application/json" --data '{"score": 1}' localhost:3000/fact/{}/vote
+    seq 5 11 | xargs -I {} -n1 -P4 curl --request POST -H "Content-Type: application/json" --data '{"score": 1}' localhost:3000/fact/{}/vote
     
     # curl -F "image=@./data/morning.jpg" localhost:3000/upload
 
