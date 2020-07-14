be33018bf0ede5cab3be7c4734497f81ef370e9d
Zhuowei Li <zli58@ncsu.edu>
2020-07-13 22:27:33 -0400

    Use promisify to simplify code

--- meow.io/routes/index.js
+++ meow.io/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const redis = require('redis');
+const util = require('util');
 let client = redis.createClient(6379, '127.0.0.1', {});
 
 const db = require('../data/db');
@@ -11,24 +12,21 @@ const RECENT_CAT_KEY = "recent_cat_key";
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   
-  let bestFacts = await new Promise((resolve) => {
-    client.get(BEST_FACT_KEY, (err, reply) => {
-      resolve(reply ? JSON.parse(reply) : null);
-    });
-  });
+  const client_get = util.promisify(client.get).bind(client);
+  let bestFacts = await client_get(BEST_FACT_KEY);
+  
   if (bestFacts == null) {
     console.log("Load bestFacts from db");
-    bestFacts = (await db.votes()).slice(0,100);
+    bestFacts = (await db.votes()).slice(0, 100);
     client.set(BEST_FACT_KEY, JSON.stringify(bestFacts));
     client.expire(BEST_FACT_KEY, 10);
   }
+  else {
+    bestFacts = JSON.parse(bestFacts);
+  }
 
-  let recentUploads = await new Promise((resolve) => {
-    client.lrange(RECENT_CAT_KEY, 0, 4, (err, reply) => {
-      // console.log(reply);
-      resolve(reply);
-    });
-  });
+  const client_lrange = util.promisify(client.lrange).bind(client);
+  let recentUploads = await client_lrange(RECENT_CAT_KEY, 0, 4);
 
   res.render('index', { title: 'meow.io', recentUploads: recentUploads, bestFacts: bestFacts});
 });
